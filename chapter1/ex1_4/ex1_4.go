540607e2224c0787fba0443170fcd97586a6e2b6
slemeshaev <lemeshaevs@gmail.com>
2025-10-10 21:40:59 +0300

    Modified dup2 to print the names of all files in which each duplicated line occurs.

--- chapter1/ex1_4/ex1_4.go
+++ chapter1/ex1_4/ex1_4.go
@@ -7,12 +7,44 @@ import (
 	"os"
 )
 
+type FileData struct {
+	counts map[string]int
+	files  map[string][]string
+}
+
+func NewFileData() *FileData {
+	return &FileData{
+		counts: make(map[string]int),
+		files:  make(map[string][]string),
+	}
+}
+
+func (data *FileData) findDuplicates(f *os.File, filename string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		line := input.Text()
+		data.counts[line]++
+
+		exists := false
+		for _, existingFile := range data.files[line] {
+			if existingFile == filename {
+				exists = true
+				break
+			}
+		}
+
+		if !exists {
+			data.files[line] = append(data.files[line], filename)
+		}
+	}
+}
+
 func main() {
-	counts := make(map[string]int)
+	fileData := NewFileData()
 	files := os.Args[1:]
 
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		fileData.findDuplicates(os.Stdin, "stdin")
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -20,22 +52,14 @@ func main() {
 				fmt.Fprintf(os.Stderr, "ex1_4: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			fileData.findDuplicates(f, arg)
 			f.Close()
 		}
 	}
 
-	for line, n := range counts {
+	for line, n := range fileData.counts {
 		if n > 1 {
-			fmt.Printf("%s\t%d\n", line, n)
+			fmt.Printf("%d\t%s\t%v\n", n, line, fileData.files[line])
 		}
 	}
-}
-
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-	// NOTE: ignoring potential errors from input.Err()
 }
