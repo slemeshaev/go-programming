4678abd55c9a33acf82f4ebd563d6bd0cbcb3179
slemeshaev <lemeshaevs@gmail.com>
2025-11-19 22:24:08 +0300

    Added types, constants, and functions to tempconv for processing temperatures // in the Kelvin scale, where zero Kelvin is -273.15°C and a difference of 1K has the same magnitude as 1°C

--- chapter2/tempconv/tempconv.go
+++ chapter2/tempconv/tempconv.go
@@ -10,11 +10,18 @@ import (
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
+)
+
+const (
+	AbsoluteZeroK Kelvin = 0
+	FreezingK     Kelvin = 273.15
+	BoilingK      Kelvin = 373.15
 )
 
 func (c Celsius) String() string {
@@ -23,4 +30,8 @@ func (c Celsius) String() string {
 
 func (f Fahrenheit) String() string {
 	return fmt.Sprintf("%g°F", f)
+}
+
+func (k Kelvin) String() string {
+	return fmt.Sprintf("%gK", k)
 }
