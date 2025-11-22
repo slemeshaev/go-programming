a986cb36a54a46e5b9a1308f565afff424887e94
slemeshaev <lemeshaevs@gmail.com>
2025-11-22 17:03:16 +0300

    Exercise 2.2

---
+++ chapter2/converter/converter.go
@@ -0,0 +1,13 @@
+// Exercise 2.2: Write a general-purpose unit-conversion program analogous
+// to cf that reads numbers from its command-line arguments or from the
+// standard input if there are no arguments, and converts each number into
+// units like temperature in Celsius and Fahreneit, length in feet and meters,
+// weight in pounds and kilograms, and the like.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Exercise 2.2")
+}
