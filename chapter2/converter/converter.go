d5c85110fb04ec318d4d30d0d52696a079270a27
slemeshaev <lemeshaevs@gmail.com>
2025-12-04 22:00:29 +0300

    Temperature types and conversions

--- chapter2/converter/converter.go
+++ chapter2/converter/converter.go
@@ -4,10 +4,47 @@
 // units like temperature in Celsius and Fahreneit, length in feet and meters,
 // weight in pounds and kilograms, and the like.
 
-package main
+package converter
 
 import "fmt"
 
-func main() {
-	fmt.Printf("Exercise 2.2")
+// Temperature types and conversions
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%.2f°C", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%.2f°F", f)
+}
+
+func (k Kelvin) String() string {
+	return fmt.Sprintf("%.2fK", k)
+}
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c + 273.15)
+}
+
+func KToC(k Kelvin) Celsius {
+	return Celsius(k - 273.15)
+}
+
+func FToK(f Fahrenheit) Kelvin {
+	return CToK(FToC(f))
+}
+
+func KToF(k Kelvin) Fahrenheit {
+	return CToF(KToC(k))
 }
