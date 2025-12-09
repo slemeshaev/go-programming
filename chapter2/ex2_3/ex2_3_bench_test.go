154275ee7547f611bffd464073b19b54211058f7
slemeshaev <lemeshaevs@gmail.com>
2025-12-09 22:30:38 +0300

    Added performence measurement of two functions

---
+++ chapter2/ex2_3/ex2_3_bench_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"tempconv/chapter2/popcount"
+	"testing"
+)
+
+func BenchmarkPopCount1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCount(100) // 0.790s
+	}
+}
+
+func BenchmarkPopCount2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popCount(100) // 1.775s
+	}
+}
