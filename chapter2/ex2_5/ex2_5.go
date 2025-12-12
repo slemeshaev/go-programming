978d80867cd4f9489818faffe2318901f8a57f8c
slemeshaev <lemeshaevs@gmail.com>
2025-12-12 21:42:23 +0300

    Add a condition of the exercise 2.5

---
+++ chapter2/ex2_5/ex2_5.go
@@ -0,0 +1,11 @@
+// Exercise 2.5: The expression x&(x-1) clears the rightmost non-zero bit of x.
+// Write a version of PopCount that counts bits by using this fact, and
+// access it performance.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
