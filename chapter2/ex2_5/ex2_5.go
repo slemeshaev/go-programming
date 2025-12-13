7d0d26f9ba62777569c370bfc5b42ae66296c362
slemeshaev <lemeshaevs@gmail.com>
2025-12-13 20:12:18 +0300

    Kernighan's algorithm

--- chapter2/ex2_5/ex2_5.go
+++ chapter2/ex2_5/ex2_5.go
@@ -4,8 +4,32 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// pc[i] is the population count of i
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count
+// (number of set bits) of x.
+func popCount(x uint64) int {
+	count := 0
+
+	for x != 0 {
+		x = x & (x - 1)
+		count++
+	}
+
+	return count
+}
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println(popCount(10))
 }
