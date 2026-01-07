254619fd5891ed069ca7129653043a23bad92276
slemeshaev <lemeshaevs@gmail.com>
2026-01-07 21:03:25 +0300

    Shift operator variables

--- chapter3/intnumbers/intnumbers.go
+++ chapter3/intnumbers/intnumbers.go
@@ -8,4 +8,10 @@ func main() {
 
 	var i int8 = 127
 	fmt.Println(i, i+1, i*i) // 127 -128 1
+
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+
+	fmt.Printf("%08b\n", x)
+	fmt.Printf("%08b\n", y)
 }
