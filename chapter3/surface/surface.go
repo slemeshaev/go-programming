857a93e2ff49a082a189e5289dcadbbd4df5fad7
slemeshaev <lemeshaevs@gmail.com>
2026-01-14 20:16:56 +0300

    The function corner returns two values, the coordinates of the corner of the cell

--- chapter3/surface/surface.go
+++ chapter3/surface/surface.go
@@ -17,3 +17,20 @@ var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30*), cos(30*)
 func main() {
 	//
 }
+
+func corner(i, j int) (float64, float64) {
+	// ищем угловую точку (x, y) ячейки (i, j)
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+	// вычисялем высоту поверхности z
+	z := f(x, y)
+	// изметрически проецируем (x, y, z) на двумерную канву SVG (sx, sy)
+	sx := width/2 + (x+y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // расстояние от (0, 0)
+	return math.Sin(r) / r
+}
