69b34fe838e4f9d37ea7e7431d11842873eeda37
slemeshaev <lemeshaevs@gmail.com>
2026-01-26 22:59:41 +0300

    Add the mandelbrot file and constants

---
+++ chapter3/mandelbrot/mandelbrot.go
@@ -0,0 +1,9 @@
+// Mandelbrot создает PNG-изображение фрактала Мандельброта.
+package main
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+}
