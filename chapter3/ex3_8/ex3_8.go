2ffcb903bae35fcedcdbfff2347597856d00bbcf
slemeshaev <lemeshaevs@gmail.com>
2026-02-05 20:28:57 +0300

    Implementation complex64 function

--- chapter3/ex3_8/ex3_8.go
+++ chapter3/ex3_8/ex3_8.go
@@ -14,41 +14,34 @@ package main
 import (
 	"image"
 	"image/color"
-	"image/png"
-	"math/cmplx"
-	"os"
 )
 
 func main() {
-	const (
-		xmin, ymin, xmax, ymax = -2, -2, +2, +2
-		width, height          = 1024, 1024
-	)
-
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	for py := 0; py < height; py++ {
-		y := float64(py)/height*(ymax-ymin) + ymin
-		for px := 0; px < width; px++ {
-			x := float64(px)/width*(xmax-xmin) + xmin
-			z := complex(x, y)
-			// image point (px, py) represents complex value z
-			img.Set(px, py, mandelbrot(z))
-		}
-	}
-	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+	// Implementation
 }
 
-func mandelbrot(z complex128) color.Color {
-	const iterations = 200
-	const contrast = 15
+// 1. complex64
+func render64() *image.RGBA {
+	const size = 256
+	img := image.NewRGBA(image.Rect(0, 0, size, size))
 
-	var v complex128
-	for n := uint8(0); n < iterations; n++ {
-		v = v*v + z
-		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
+			cx := float32(x)/size*4 - 2
+			cy := float32(y)/size*4 - 2
+			img.Set(x, y, mandel64(complex(cx, cy)))
 		}
 	}
+	return img
+}
 
+func mandel64(c complex64) color.Color {
+	var z complex64
+	for n := 0; n < 100; n++ {
+		z = z*z + c
+		if real(z)*real(z)+imag(z)*imag(z) > 4 {
+			return color.Gray{255 - uint8(n*2)}
+		}
+	}
 	return color.Black
 }
