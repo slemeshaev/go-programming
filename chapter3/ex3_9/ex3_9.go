a243f384497369e380c76117e067611358a6334b
slemeshaev <lemeshaevs@gmail.com>
2026-02-12 20:07:41 +0300

    Implementation mandelbrot function

--- chapter3/ex3_9/ex3_9.go
+++ chapter3/ex3_9/ex3_9.go
@@ -7,6 +7,7 @@ package main
 import (
 	"image"
 	"image/color"
+	"math/cmplx"
 )
 
 func main() {
@@ -29,6 +30,16 @@ func renderMandelbrot(xmin, xmax, ymin, ymax float64, size int) *image.RGBA {
 }
 
 func mandelbrot(z complex128) color.Color {
-	// implementation
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+
 	return color.Black
 }
