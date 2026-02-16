7a39ea04139f350e9cb19f012e8d2555773f5227
slemeshaev <lemeshaevs@gmail.com>
2026-02-16 19:54:09 +0300

    To append one string to another

--- chapter3/strings/strings.go
+++ chapter3/strings/strings.go
@@ -8,7 +8,7 @@ func main() {
 	fmt.Println(len(s))     // "12"
 	fmt.Println(s[0], s[7]) // "104 119" ('h' and 'w')
 
-	c := s[len(s)] // panic: index out of range
+	c := s[len(s)-1] // panic: index out of range
 	fmt.Println(c)
 
 	fmt.Println(s[0:5]) // "hello"
@@ -19,4 +19,13 @@ func main() {
 
 	// The + operator makes a new string by concatenating two strings
 	fmt.Println("goodbye" + s[5:]) // "goodbye, world"
+
+	foot := "left foot"
+	t := foot
+	foot += ", right foot"
+
+	fmt.Println(foot) // "left foot, right foot"
+	fmt.Println(t)    // "left foot"
+
+	// foot[0] = 'L' // compile error: cannot assign to s[0]
 }
