3974de10878e7b70a18c8919c162f7b28eb311a9
slemeshaev <lemeshaevs@gmail.com>
2026-02-24 14:06:45 +0300

    Comma inserts commas in a non-negative decimal integer string

---
+++ chapter3/comma/comma.go
@@ -0,0 +1,19 @@
+// comma inserts commas in a non-negative decimal integer string
+package main
+
+import "fmt"
+
+func comma(s string) string {
+	n := len(s)
+
+	if n <= 3 {
+		return s
+	}
+
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func main() {
+	str := "12345"
+	fmt.Println(comma(str))
+}
