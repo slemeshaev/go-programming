6d37c4030057a905e94e1ee90f655561c8a8703a
slemeshaev <lemeshaevs@gmail.com>
2026-02-26 20:12:33 +0300

    Add the task of the a non-recursive version of comma

---
+++ chapter3/ex3_10/ex3_10.go
@@ -0,0 +1,16 @@
+// Exercise 3.10: Write a non-recursive version of comma,
+// using bytes.Buffer instead of string concatenation.
+
+package main
+
+import "fmt"
+
+func comma(s string) string {
+	// implementation
+	return s
+}
+
+func main() {
+	str := "12345"
+	fmt.Println(comma(str))
+}
