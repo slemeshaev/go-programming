b202b57ff2e24851a3334583ec77aecc9fc8b17c
slemeshaev <lemeshaevs@gmail.com>
2026-02-27 18:11:09 +0300

    Implementation a non-recursive version of comma

--- chapter3/ex3_10/ex3_10.go
+++ chapter3/ex3_10/ex3_10.go
@@ -3,11 +3,32 @@
 
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 func comma(s string) string {
-	// implementation
-	return s
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+
+	var buf bytes.Buffer
+
+	remainder := n % 3
+	if remainder == 0 {
+		remainder = 3
+	}
+
+	buf.WriteString(s[:remainder])
+
+	for i := remainder; i < n; i += 3 {
+		buf.WriteByte(',')
+		buf.WriteString(s[i : i+3])
+	}
+
+	return buf.String()
 }
 
 func main() {
