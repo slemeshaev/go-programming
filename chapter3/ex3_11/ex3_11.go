ab0e3a51399ae622b2cc21633470fec15d3d4bf1
slemeshaev <lemeshaevs@gmail.com>
2026-02-28 09:40:19 +0300

    Add a non-recursive version of comma. It is a comma Integer

---
+++ chapter3/ex3_11/ex3_11.go
@@ -0,0 +1,59 @@
+// Exercise 3.11: Enhance comma so that it deals
+// correctly with floating-point numbers and an optional sign.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func commaInteger(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+
+	var buf bytes.Buffer
+	remainder := n % 3
+	if remainder == 0 {
+		remainder = 3
+	}
+
+	buf.WriteString(s[:remainder])
+
+	for i := remainder; i < n; i += 3 {
+		buf.WriteByte(',')
+		buf.WriteString(s[i : i+3])
+	}
+
+	return buf.String()
+}
+
+func comma(s string) string {
+	// implementation
+	return s
+}
+
+func main() {
+	testCases := []string{
+		"123",
+		"12345",
+		"1234567",
+		"+1234567",
+		"-1234567",
+		"12345.6789",
+		"-12345.6789",
+		"+12345.6789",
+		"1234567890.12345",
+		"-1234567890.12345",
+		"0.123",
+		"-0.123",
+		".123",
+		"-.123",
+	}
+
+	for _, tc := range testCases {
+		fmt.Printf("%-20s -> %s\n", tc, comma(tc))
+	}
+}
