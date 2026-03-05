8c645a83f38df525e1c4e4c01e3cde4e8491d706
slemeshaev <lemeshaevs@gmail.com>
2026-03-05 19:23:11 +0300

    Add the functions that test, set, or clear one or more of these bits

---
+++ chapter3/netflag/netflag.go
@@ -0,0 +1,31 @@
+package main
+
+type Flags uint
+
+const (
+	FlagUp Flags = 1 << iota // is up
+	FlagBroadcast
+	FlagLoopback
+	FlagPointToPoint
+	FlagMulticast
+)
+
+func IsUp(v Flags) bool {
+	return v&FlagUp == FlagUp
+}
+
+func TurnDown(v *Flags) {
+	*v &^= FlagUp
+}
+
+func SetBroadcast(v *Flags) {
+	*v |= FlagBroadcast
+}
+
+func IsCast(v Flags) bool {
+	return v&(FlagBroadcast|FlagMulticast) != 0
+}
+
+func main() {
+	// examples...
+}
