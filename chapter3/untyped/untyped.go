b60c253a37cf807b6ab2ca38b1aef752ee8f2fc5
slemeshaev <lemeshaevs@gmail.com>
2026-03-09 10:31:52 +0300

    Examples of using untyped constants

--- chapter3/untyped/untyped.go
+++ chapter3/untyped/untyped.go
@@ -16,4 +16,23 @@ func main() {
 	fmt.Println((f - 32) * 5 / 9)
 	fmt.Println(5 / 9 * (f - 32))
 	fmt.Println(5.0 / 9.0 * (f - 32))
+
+	var g float64 = 3 + 0i // untyped complex -> float64
+	g = 2                  // untyped integer -> float64
+	g = 1e123              // untyped floating point -> float64
+	g = 'a'                // untyped rune -> float64
+
+	fmt.Println(g)
+
+	var h float64 = float64(3 + 0i)
+	h = float64(2)
+	h = float64(1e123)
+	h = float64('a')
+
+	fmt.Println(h)
+
+	fmt.Printf("%T\n", 0)
+	fmt.Printf("%T\n", 0.0)
+	fmt.Printf("%T\n", 0i)
+	fmt.Printf("%T\n", '\000')
 }
