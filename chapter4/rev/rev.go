668b3115f4d5bd19eb48e7547a40ebe0c614e0d8
slemeshaev <lemeshaevs@gmail.com>
2026-03-17 22:14:24 +0300

    reverses the order of number in place

---
+++ chapter4/rev/rev.go
@@ -0,0 +1,12 @@
+package main
+
+// reverse обращает порядок чисел "на месте"
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	//
+}
