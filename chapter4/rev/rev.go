cc19cd5f1867feddb2aaf78af9965e1335671cf1
slemeshaev <lemeshaevs@gmail.com>
2026-03-18 20:15:36 +0300

    reverse reverses a slice of ints in place

--- chapter4/rev/rev.go
+++ chapter4/rev/rev.go
@@ -1,6 +1,8 @@
 package main
 
-// reverse обращает порядок чисел "на месте"
+import "fmt"
+
+// reverse reverses a slice of ints in place
 func reverse(s []int) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
@@ -8,5 +10,7 @@ func reverse(s []int) {
 }
 
 func main() {
-	//
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(a[:])
+	fmt.Println(a) // [5 4 3 2 1 0]
 }
