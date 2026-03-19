ef969806ee82d012ef892761ce451e58e58448c2
slemeshaev <lemeshaevs@gmail.com>
2026-03-19 20:13:27 +0300

    Rotate s left by two positions

--- chapter4/rev/rev.go
+++ chapter4/rev/rev.go
@@ -13,4 +13,11 @@ func main() {
 	a := [...]int{0, 1, 2, 3, 4, 5}
 	reverse(a[:])
 	fmt.Println(a) // [5 4 3 2 1 0]
+
+	s := [...]int{0, 1, 2, 3, 4, 5}
+	// Rotate s left by two positions
+	reverse(s[:2])
+	reverse(s[2:])
+	// reverse(s)
+	fmt.Println(s) // [2 3 4 5 0 1]
 }
