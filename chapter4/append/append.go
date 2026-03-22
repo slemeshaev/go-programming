c92b82a60791effd80244685bf534b88c5ad03ec
slemeshaev <lemeshaevs@gmail.com>
2026-03-22 18:14:39 +0300

    Each change in capacity indicates an allocation and a copy

--- chapter4/append/append.go
+++ chapter4/append/append.go
@@ -7,14 +7,17 @@ func appendInt(x []int, y int) []int {
 	zlen := len(x) + 1
 
 	if zlen <= cap(x) {
+		// There is room to grow. Extend the slice.
 		z = x[:zlen]
 	} else {
+		// There is insufficient space. Allocate a new array.
+		// Grow by doubling, for amortized linear complexity.
 		zcap := zlen
 		if zcap < 2*len(x) {
 			zcap = 2 * len(x)
 		}
 		z = make([]int, zlen, zcap)
-		copy(z, x)
+		copy(z, x) // a built-in function; see text
 	}
 
 	z[len(x)] = y
@@ -28,4 +31,11 @@ func main() {
 	}
 
 	fmt.Printf("%q\n", runes)
+
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d cap = %d\t%v\n", i, cap(y), y)
+		x = y
+	}
 }
