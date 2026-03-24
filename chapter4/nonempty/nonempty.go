5fb83ad3bb7060130d28161e68b0c6284c1a54a9
slemeshaev <lemeshaevs@gmail.com>
2026-03-24 20:07:46 +0300

    zero-length slice of original

--- chapter4/nonempty/nonempty.go
+++ chapter4/nonempty/nonempty.go
@@ -16,6 +16,16 @@ func nonempty(strings []string) []string {
 	return strings[:i]
 }
 
+func nonempty2(strings []string) []string {
+	out := strings[:0] // zero-length slice of original
+	for _, s := range strings {
+		if s != "" {
+			out = append(out, s)
+		}
+	}
+	return out
+}
+
 func main() {
 	data := []string{"one", "", "three"}
 	fmt.Printf("%q\n", nonempty(data))
