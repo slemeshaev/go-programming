7eeda311bc396b7df77c72685a73e0d277443e5b
slemeshaev <lemeshaevs@gmail.com>
2026-03-31 19:48:23 +0300

    Remove an unnecessary function

--- chapter4/ex4_7/ex4_7.go
+++ chapter4/ex4_7/ex4_7.go
@@ -9,12 +9,6 @@ import (
 	"unicode/utf8"
 )
 
-func reverseBytes(b []byte) {
-	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
-	}
-}
-
 func reverseUTF8(b []byte) []byte {
 	runes := make([]rune, 0, utf8.RuneCount(b))
 	for i := 0; i < len(b); {
