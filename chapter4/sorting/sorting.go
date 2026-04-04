c66956a3e6b6d46a35156006cc00535885d6bb35
slemeshaev <lemeshaevs@gmail.com>
2026-04-04 09:40:10 +0300

    Checking whether an element is in the map or not

--- chapter4/sorting/sorting.go
+++ chapter4/sorting/sorting.go
@@ -7,7 +7,6 @@ import (
 
 func main() {
 	ages := make(map[string]int)
-	ages["bob"] = 43
 	ages["alice"] = 31
 	ages["charlie"] = 34
 
@@ -20,5 +19,18 @@ func main() {
 	sort.Strings(names)
 	for _, name := range names {
 		fmt.Printf("%s\t%d\n", name, ages[name])
+	}
+
+	age, ok := ages["bob"]
+	if !ok {
+		fmt.Println("bob is not key in this map")
+	} else {
+		fmt.Printf("%s is a key in this map %d\n", "bob", age)
+	}
+
+	if age, ok := ages["alice"]; !ok {
+		fmt.Println("alice is not key in this map")
+	} else {
+		fmt.Printf("%s is a key in this map %d\n", "alice", age)
 	}
 }
