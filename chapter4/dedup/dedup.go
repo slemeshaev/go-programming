144acf59d1ba4d9ecd04a41457c11b2178a5aa78
slemeshaev <lemeshaevs@gmail.com>
2026-04-06 20:16:35 +0300

    Extra output of maps

--- chapter4/dedup/dedup.go
+++ chapter4/dedup/dedup.go
@@ -14,12 +14,17 @@ func main() {
 		line := input.Text()
 		if !seen[line] {
 			seen[line] = true
-			fmt.Println(line)
 		}
 	}
 
 	if err := input.Err(); err != nil {
 		fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
 		os.Exit(1)
+	}
+
+	fmt.Printf("Seen: %v\n", seen)
+
+	for k, v := range seen {
+		fmt.Printf("%s = %t\n", k, v)
 	}
 }
