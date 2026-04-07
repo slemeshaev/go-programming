73f845a455952dcda4aa58930a79fa2c0ca21e2a
slemeshaev <lemeshaevs@gmail.com>
2026-04-07 22:34:10 +0300

    This example uses a map to record the number of times Add has been called wit a given list of strings

--- chapter4/dedup/dedup.go
+++ chapter4/dedup/dedup.go
@@ -6,6 +6,20 @@ import (
 	"os"
 )
 
+var m = make(map[string]int)
+
+func k(list []string) string {
+	return fmt.Sprintf("%q", list)
+}
+
+func Add(list []string) {
+	m[k(list)]++
+}
+
+func Count(list []string) int {
+	return m[k(list)]
+}
+
 func main() {
 	seen := make(map[string]bool) // a set of strings
 	input := bufio.NewScanner(os.Stdin)
